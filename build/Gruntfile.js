fcd4d5f758efd665b94c2f2e9d6c097f55a67d51
Pedro Ferreira <plferreira4@gmail.com>
2015-07-30 08:36:05 +0100

    - add a build script with gruntjs - fix, add missing 'tetris_icon.png' to repository

---
+++ build/Gruntfile.js
@@ -0,0 +1,78 @@
+module.exports = function( grunt )
+{
+var root = '../';
+var dest = '../release/<%= pkg.name %>_<%= pkg.version %>/';
+
+grunt.initConfig({
+        pkg: grunt.file.readJSON( 'package.json' ),
+
+            // remove the destination folder first
+        clean: {
+            options: {
+                force: true
+            },
+
+            release: [
+                dest
+            ]
+        },
+
+            // copy the necessary files
+        copy: {
+            release: {
+                expand: true,
+                cwd: root,
+                src: [
+                    'libraries/**'
+                ],
+                dest: dest
+            }
+        },
+
+            // minimize the javascript
+        uglify: {
+            release: {
+                files: [{
+                    src: [
+                        root + 'scripts/utilities.js',
+                        root + 'scripts/piece.js',
+                        root + 'scripts/*.js'
+                    ],
+                    dest: dest + 'min.js'
+                }]
+            }
+        },
+
+            // minimize the css
+        cssmin: {
+            release: {
+                files: [{
+                    expand: true,
+                    cwd: root + 'css/',
+                    src: '*.css',
+                    dest: dest + 'css/'
+                }]
+            }
+        },
+
+            // update the html file to load the min.js file
+        processhtml: {
+            release: {
+                expand: true,
+                cwd: root,
+                src: 'index.html',
+                dest: dest
+            }
+        }
+    });
+
+    // load the plugins
+grunt.loadNpmTasks( 'grunt-contrib-clean' );
+grunt.loadNpmTasks( 'grunt-contrib-copy' );
+grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
+grunt.loadNpmTasks( 'grunt-processhtml' );
+
+    // tasks
+grunt.registerTask( 'default', [ 'clean', 'copy', 'uglify', 'cssmin', 'processhtml' ] );
+};
