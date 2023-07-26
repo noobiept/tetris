ed712aadd7a160b28ccdc250bf531273f9f44098
Pedro Ferreira <plferreira4@gmail.com>
2023-07-26 22:51:23 +0200

    ✨: Add a linter to the project (eslint).

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+/* eslint-env node */
+module.exports = {
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/jsx-runtime",
+        "plugin:react-hooks/recommended",
+    ],
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+    root: true,
+    ignorePatterns: ["node_modules", "release", "build"],
+};
