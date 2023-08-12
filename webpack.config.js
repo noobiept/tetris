eeca0d7e03a9b2dcb95a27d039bb39a33771fefe
Pedro Ferreira <plferreira4@gmail.com>
2023-08-12 19:50:25 +0200

    🔧: Add a circular dependency detection plugin to the build.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require("path");
 const package = require("./package.json");
+const CircularDependencyPlugin = require("circular-dependency-plugin");
 
 module.exports = {
     entry: "./source/index.tsx",
@@ -39,6 +42,19 @@ module.exports = {
                 { from: "./libraries/", to: "./libraries/" },
                 { from: "./assets/", to: "./assets/" },
             ],
+        }),
+        new CircularDependencyPlugin({
+            // exclude detection of files based on a RegExp
+            exclude: /a\.js|node_modules/,
+            // include specific files based on a RegExp
+            include: /source/,
+            // add errors to webpack instead of warnings
+            failOnError: true,
+            // allow import cycles that include an asyncronous import,
+            // e.g. via import(/* webpackMode: "weak" */ './file.js')
+            allowAsyncCycles: false,
+            // set the current working directory for displaying module paths
+            cwd: process.cwd(),
         }),
     ],
 };
